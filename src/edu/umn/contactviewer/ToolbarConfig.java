08c16be848bbd46bf5837bab9005d225381afa9e
xin <xin@192.168.1.5>
2013-02-28 08:26:13 -0600

    remove unused import

--- src/edu/umn/contactviewer/ToolbarConfig.java
+++ src/edu/umn/contactviewer/ToolbarConfig.java
@@ -1,8 +1,6 @@
 package edu.umn.contactviewer;
 
 import android.app.Activity;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
